cd0b72cab7e0c38fc553266690a058e010441127
unknown <gary.kim.1009@gmail.com>
2023-02-04 04:13:34 +1300

    removed location function from App.js

--- baram/src/App.js
+++ baram/src/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import MainScreen from './Components/Features/MainScreen.js';
-import Location from './Components/Features/Location.js';
 
 function App(){
   return(
