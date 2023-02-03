b42592e60d1b956717d692f86f1cffaaa31f034a
unknown <gary.kim.1009@gmail.com>
2023-02-03 21:24:12 +1300

    added MainScreen page

--- baram/src/App.js
+++ baram/src/App.js
@@ -1,34 +1,9 @@
-import './App.css'
 import * as React from 'react';
-import { Button, SafeAreaView, Text } from 'react-native';
+import MainScreen from './Components/Features/MainScreen.js';
 
-function MainScreen({navigation}){
+function App(){
   return(
-    <SafeAreaView>
-      <Text>Welcome to Baram</Text> 
-      <Button title="Refresh" />
-      <Button title="Map" />
-      <Button title="Settings" />
-    </SafeAreaView>
-  )
-}
-
-
-function App() {
-  return (
-    <view className="App">
-      <h1>Welcome to Baram</h1>
-      <p>Today's weather is:
-      <br />
-      It is x degrees 
-      <br />
-      It will feel like x degrees
-      <br />
-      Wind: 
-      <br />
-      Humidity:
-      </p>
-    </view>
+    <MainScreen />
   );
 }
 
