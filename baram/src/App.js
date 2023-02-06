c4daa01efa7696d5916df1ecc6bcbc9b88de3bea
unknown <gary.kim.1009@gmail.com>
2023-02-07 00:06:02 +1300

    tidyup

--- baram/src/App.js
+++ baram/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import MainScreen from './Components/Features/MainScreen.js';
+import MainScreen from './components/Features/MainScreen.js';
 
 function App(){
   return(
