64e368d53cbf27beafbb6998c793df3794f0739f
unknown <gary.kim.1009@gmail.com>
2023-02-07 00:41:41 +1300

    added styling to title font

--- baram/src/Components/Features/Location.js
+++ baram/src/Components/Features/Location.js
@@ -1,11 +1,32 @@
 //will have function here to export the location from API 
+//https://www.npmjs.com/package/react-native-weather-apihttps://www.npmjs.com/package/react-native-weather-api
 import * as React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+  } from 'react-native';
+
 
 //run the Location function onclick
 export default function Location(){
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Refresh</Text>
+      <View style = {styles.pageTitle} >
+        <text style={styles.title}>Welcome to Baram</text>
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+  pageTitle: {
+    flex: 1,
+  },
+    title: {
+    marginTop: 16,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
