8767362252be08272d16c6ecd96749351529cc09
unknown <gary.kim.1009@gmail.com>
2023-02-08 00:05:27 +1300

    optimised how to load the icons as per lecture but am unable to workout how to load icons when an app is not in focus

--- baram/src/Components/Features/MainScreen.js
+++ baram/src/Components/Features/MainScreen.js
@@ -8,38 +8,34 @@ import SettingsScreen from './SettingsScreen';
 import {Ionicons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+const Tab_icon = {
+  //after optimising I am unsure how to change the icon based on it's focus
+  Main: 'md-cloud',
+  Map: 'md-map',
+  Settings: 'md-settings',
+
+}
+
+const createScreenOptions = ({route}) => {
+  const iconName = Tab_icon[route.name]
+  return {
+    tabBarIcon: ({size, color}) => (
+      //set configuration of the navigation bar 
+      <Ionicons name={iconName} size={25} color={color}/>
+    ),
+  };
+};
 
 export default function MainScreen() {
   return (
       <NavigationContainer>
         <Tab.Navigator
-          screenOptions = {({route}) => ({
-            tabBarIcon: ({ focused, color, size}) => {
-              let iconName;
-              //set icon based on Screen Name
-              
-              //if the focused screen is Main display the refresh icon to tell users they can refresh the location else cloud icon so they can 'reopen' the screen
-              if (route.name === 'Main'){
-                  iconName = focused
-                  ? 'ios-refresh'
-                  : 'ios-cloud';
-              
-              //if the screen is map display map icon regardless of focus 
-              }else if (route.name === 'Map'){
-                iconName="md-map";
-
-              //if the screen is settings
-              }else if (route.name === 'Settings'){
-                iconName="md-settings";
-              }
-
-              //set the configurations of the
-              return <Ionicons name={iconName} size={25} color={color}/>;
-            },
+          screenOptions = {createScreenOptions} 
+          tabBarOptions={{
             //set the color of the icons based on whether they are active or inactive 
             tabBarActiveTintColor: '#32bef6',
             tabBarInactiveTintColor: 'gray',
-          })}
+          }}
         >
           <Tab.Screen name='Main' component={Location} />
           <Tab.Screen name='Map' component={MapScreen} />
