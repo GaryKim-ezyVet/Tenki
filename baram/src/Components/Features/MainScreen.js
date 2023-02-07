202ca647fdbc6c8f1cb5626b11fcaa8be2ed32f2
unknown <gary.kim.1009@gmail.com>
2023-02-07 23:28:55 +1300

    made changes to the Mainscreen - added icons to the navigation panel

--- baram/src/Components/Features/MainScreen.js
+++ baram/src/Components/Features/MainScreen.js
@@ -5,19 +5,45 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Location from './Location';
 import MapScreen from './MapScreen';
 import SettingsScreen from './SettingsScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
+
 export default function MainScreen() {
   return (
-    <View>
-      <Text>Welcome to Baram</Text>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Main" component={Location} />
-          <Tab.Screen name="Map" component={MapScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Navigator
+          screenOptions = {({route}) => ({
+            tabBarIcon: ({ focused, color, size}) => {
+              let iconName;
+              //set icon based on Screen Name
+              
+              //if the screen is Main
+              if (route.name === 'Main'){
+                  iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              //if the screen is map
+              }else if (route.name === 'Map'){
+                iconName=focused ? 'ios-map' : 'ios-map';
+
+              //if the screen is settings
+              }else if (route.name === 'Settings'){
+                iconName=focused ? 'ios-list' : 'ios-list-outline';
+              }
+
+              //set the configurations of the
+              return <Ionicons name={iconName} size={25} color={color}/>;
+            },
+            //set the color of the icons based on whether they are active or inactive 
+            tabBarActiveTintColor: '#32bef6',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name='Main' component={Location} />
+          <Tab.Screen name='Map' component={MapScreen} />
+          <Tab.Screen name='Settings' component={SettingsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
-    </View>
   );
 }
