cf68a3295221f1e9871f1e2b075336ab763bc69f
unknown <gary.kim.1009@gmail.com>
2023-02-22 23:51:31 +1300

    update api url and forecast variable with if clause

--- baram/components/Features/Location.js
+++ baram/components/Features/Location.js
@@ -2,27 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
 import * as Location from 'expo-location';
 
-const api = {
   
 //weather key from openweather api
-openWeatherKey: 'bb481abe6d37c9527b03cf0575897349',
-base_weather_api_url: 'https://api.openweathermap.org/data/2.5/weather?',
-}
+const openWeatherKey = 'bb481abe6d37c9527b03cf0575897349';
+const base_weather_api_url = 'https://api.openweathermap.org/data/2.5/weather?';
+
 
 //user expo location and weather api to load weather details for current location.
-
 export default function Applocation() {
   console.log('app started');
-  const [forecast, setForecast] = useState({});
+  const [forecast, setForecast] = useState(null);
   const [globalLat, setglobalLat] = useState([]);
   const [globalLon, setglobalLon] = useState([]);
-
+  console.log('start app');
+  
   useEffect(() => {
-
-    loadWeather;  
-    }, []);
-  const loadWeather = async () => {
-    
+    loadWeather
+  }, []);
+    const loadWeather = async () => {
+    console.log('start async function')
     //Access permissions to access location services 
     let { status } = await Location.requestForegroundPermissionsAsync();
     let globalPositioning = await Location.getCurrentPositionAsync({});
@@ -31,36 +29,38 @@ export default function Applocation() {
     setglobalLat(globalPositioning.coords.latitude);
     setglobalLon(globalPositioning.coords.longitude);
     
-
+    console.log('Global positioning: ', globalPositioning);
     //fetch information from the API then assign the value to variable Forecast
-    fetch('${api.base_weather_api_url}lat=${globalLat}&lon=${globalLon}&units=metric&APPID=${api.openWeatherKey}')
+    console.log('lon:', globalLon);
+    console.log('lat:', globalLat);
+
+    //fetch(`${base_weather_api_url}lat=${globalLat}?&lon=${globalLon}?&units=metric&APPID=${openWeatherKey}`)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.4226711&lon=-122.0849872&appid=bb481abe6d37c9527b03cf0575897349')
       .then((res) => res.json())
       .then((result) => {
-        setForecast(result);
-      })
+      setForecast(result)
+      console.log('result',result)
+    })
+    
+    console.log('forecast', forecast);
   }
-  
-
 
   return (
     <View style={styles.pageTitle}>
+      
       <Text style={styles.title}> Welcome to Baram </Text>
-      <Text style={styles.contextheader}>It is currently : {forecast.main}</Text>
+      <Text>{forecast?.main.temp}</Text>
       <Image style={styles.icons} source={require('../../assets/rain.png')} />
-      <Text style={styles.context}>The current humidity is: {forecast.main.humidity}</Text>
-      <Text style={styles.context}>The current temperature is:{forecast.main.temp}</Text>  
-      <Text style={styles.context}>The current windspeed is: {forecast.wind.speed}</Text>
       <Text style={styles.contextheader}>
         {'\n'}The current weather in Wellington is: [Icon here]
         {'\n'} There is a 4% chance of rain
         {'\n'} The current temperature is: 26
         {'\n'} The current windspeed is: 12m/s
       </Text>
-
-      {/* Press to re-load weather*/}
+      
       <Button 
       title = {'Update location'} 
-      onClick = {loadWeather}
+      onPress = {loadWeather}
       />
      
     </View>
