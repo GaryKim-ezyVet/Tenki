5f82dd816f61bd9ae9fbd2bed431f5f415c1f0fe
Gary Kim <gary.kim@ezyvet.com>
2023-02-28 18:46:40 +1300

    update to code after meeting with Nate; added reusable component and added logo

--- baram/components/Features/Location.js
+++ baram/components/Features/Location.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import * as Location from 'expo-location';
   
+//Things to do / separate the code so that there is more structure (what is seen vs not) / if statement for Lat and Lon / use props and reusable components to build horizontal sliders for weather information
+
 //weather key from openweather api
 const openWeatherKey = 'bb481abe6d37c9527b03cf0575897349';
 const base_weather_api_url = 'https://api.openweathermap.org/data/2.5/weather?';
@@ -16,7 +18,7 @@ export default function Applocation() {
   const [globalLon, setglobalLon] = useState(0);
   
   console.log('start app');
-  
+    
   useEffect(() => {  
 
     const loadWeather = async () => {
@@ -26,7 +28,7 @@ export default function Applocation() {
 
     //Access permissions to access location services 
     await Location.requestForegroundPermissionsAsync();
-
+    
     //load global lat and lon from the permissions
     let globalPositioning = await Location.getCurrentPositionAsync();
     
@@ -39,6 +41,17 @@ export default function Applocation() {
     console.log('before fetch lon:', globalLon);
     console.log('before fetch lat:', globalLat);
     
+    
+  }; 
+loadWeather();
+},[]);
+
+
+  console.log('after fetch lon:', globalLon);
+  console.log('after fetch lat:', globalLat);
+
+  //set up if and else so that useEffect will only run if globalLon and Lat are set
+  useEffect(() =>{
     //https://api.openweathermap.org/data/2.5/weather?lat=-36.8581337&lon=174.754095&units=metric&appid=bb481abe6d37c9527b03cf0575897349 at the ezyVet office
     fetch(`${base_weather_api_url}lat=${globalLat}&lon=${globalLon}&units=metric&APPID=${openWeatherKey}`)
       .then((res) => res.json())
@@ -46,26 +59,29 @@ export default function Applocation() {
       setForecast(result)
       
     })
-  }; 
-loadWeather();
-},[globalLat,globalLon]);  
-
-  console.log('after fetch lon:', globalLon);
-  console.log('after fetch lat:', globalLat);
+  },[globalLat,globalLon]);
 
 
+
+
+  //icon list: https://openweathermap.org/weather-conditions
   return (
     <View style={styles.pageTitle}>
       
-      <Text style={styles.title}> Welcome to Baram </Text>
-      <Text style={styles.contextheader}>The current temperature at {forecast?.name} is: {forecast?.main.temp}</Text>
-      <Text style={styles.context}>The current weather is: {forecast?.weather[0].main} </Text>
-      <Text style={styles.context}>The current humidity is {forecast?.main.humidity}% </Text>
-      <Image style={styles.icons} source={require('../../assets/rain.png')} />
+      <Image source={require('../../assets/baram-logo.png')} />
+      <HorizontalSlider 
+      cityName = {forecast?.name}
+      cityTemp = {forecast?.main.temp}
+      cityWeather = {forecast?.weather[0].main}
+      //cityIcon = {}
+      cityHumidity = {forecast?.main.humidity}
+      />
       
-      <Button 
-      title = {'Update location'}
-      //onClick = {loadWeather}
+      <HorizontalSlider 
+      cityName = {forecast?.name}
+      cityTemp = {forecast?.main.temp}
+      cityWeather = {forecast?.weather[0].main}
+      cityHumidity = {forecast?.main.humidity}
       />
      
     </View>
@@ -73,6 +89,17 @@ loadWeather();
   
 }
 
+//Re-usable component to display weather information per city 
+//props > make sense which parts are grouped together > mental box > they become their own component > pass the data down 
+const HorizontalSlider = ({cityName, cityTemp, cityWeather, cityHumidity}) => {
+  return(
+    <View>
+      <Text style={styles.contextheader}>The current temperature at {cityName ?? 'Loading'} is: {cityTemp ?? 'Loading'}</Text>
+      <Text style={styles.context}>The current weather is: {cityWeather ?? 'Loading'} </Text>
+      <Text style={styles.context}>The current humidity is {cityHumidity ?? 'Loading'}% </Text>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   pageTitle: {
