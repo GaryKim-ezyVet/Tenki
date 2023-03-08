65cf2b486256216f075df1ad50a1ac183bf2128e
unknown <gary.kim.1009@gmail.com>
2023-03-08 23:05:31 +1300

    Split the Get Location and forecast functions

---
+++ baram/src/utils/GetLocation.js
@@ -0,0 +1,16 @@
+import * as Location from 'expo-location';
+
+
+const defaultLocation = {
+    id:0,
+    city: 'Auckland',
+    lat: -35,
+    lon: 175,
+}
+const currentLocation = null;
+
+export default async function GetLocation() {
+    await Location.requestForegroundPermissionsAsync();
+    let currentLocation = await Location.getCurrentPositionAsync();
+    return currentLocation ?? defaultLocation;
+}
--- baram/src/views/Location.js
+++ baram/src/views/Location.js
@@ -3,13 +3,18 @@ import { SafeAreaView, Image, FlatList, ScrollView } from 'react-native';
 import { WeatherDisplay } from '../components/WeatherDisplay';
 import { styles } from '../styles/styles';
 import GetForecast from '../utils/GetForecast';
+import GetLocation, {currentLocation} from '../utils/GetLocation';
+import {cityList} from '../../assets/cityList';
 
 //main screen weather icon does not load 
 //make things typescript
 
 export default function Applocation() {
+  console.log(cityList);  
+  //call current location and permissions
+  GetLocation();
 
-  const forecast = GetForecast(forecast);
+  console.log('location',currentLocation);
 
   //return a Flatlist which will load forecast into the Weather display cards if forecast value is not null
   return (
@@ -20,34 +25,10 @@ export default function Applocation() {
       />
 
       <FlatList
-      data = {forecast ? [
-        {cityName: forecast?.name, 
-          cityTemp:forecast?.main,
-          cityWeather:forecast?.weather[0].icon,
-          cityHumidity:forecast?.main.humidity
-        },
-        {cityName: 'Christchurch',
-          cityTemp: 21,
-          cityWeather: '09d',
-          cityHumidity: 60
-        },
-        {cityName: 'Wellington',
-          cityTemp: 18,
-          cityWeather: '10d',
-          cityHumidity: 30
-        },
-        {cityName: 'Seoul',
-          cityTemp: -1,
-          cityWeather: '13n',
-          cityHumidity: 4
-        },
-        {cityName: 'Tokyo',
-          cityTemp: 6,
-          cityWeather: '50n',
-          cityHumidity: 30
-        },
+      data = {[
+        {cityList}
       //could I load a function in the data here to then return theese fore units to then use in the Flat list? - this way I can pull the location with the API url then dynamically pull out the information like temp or icon etc
-      ] : []}
+      ]}
         
       renderItem = {({item}) => <WeatherDisplay cityName={item.cityName} cityTemp={item.cityTemp} cityWeather={item.cityWeather} cityHumidity={item.cityHumidity}/>}
       //should not use index here id and name combination would be best since index is not unique
